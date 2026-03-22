f42dc2e42928f574d7f147baf447af9ea1242677
MMRclear <mohamedmahmued128@gmail.com>
2026-03-22 21:55:07 +0200

    Authorize note in the project

--- controllers/note.php
+++ controllers/note.php
@@ -6,7 +6,17 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db -> query('Select * from notes where id = :id',['id' => $_GET['id']])->fetch();
+$note = $db -> query('Select * from notes where id = :id',
+[
+    'id' => $_GET['id']])->fetch();
+
+    if(! $note){
+        abort();
+    }
+
+    if($note['user_id'] !== 1){
+        abort(403);
+    }
 
 require "views/note.view.php";
  
