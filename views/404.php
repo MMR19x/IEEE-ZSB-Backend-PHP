7c6539a8e45dec1329d35337bb7b2e6c6e681058
MMRclear <mohamedmahmued128@gmail.com>
2026-03-22 02:38:45 +0200

    Add views for pages with their styles

---
+++ views/404.php
@@ -0,0 +1,12 @@
+ <?php require ('partial/head.php') ?>
+<?php require ('partial/nav.php') ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class = "text-2xl font-bold">Sorry. Page Not Found</h1>
+    <p class = "mt-4">
+      <a href ='/' class = "text-blue underline"> Go Back to Home Page </a>
+    </p>
+    </div>
+  </main>
+  <?php require ('partial/footer.php') ?>
---
+++ views/index.view.php
@@ -0,0 +1,12 @@
+<?php require ('partial/head.php') ?>
+<?php require ('partial/nav.php') ?>
+<?php require ('partial/banner.php') ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <p>
+        Hello Welcome to Home Page!!
+      </p>
+    </div>
+  </main>
+  <?php require ('partial/footer.php') ?>
