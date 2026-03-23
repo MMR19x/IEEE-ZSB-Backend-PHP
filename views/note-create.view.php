4a803ab39bc028f58868d60c91e79977f899d807
MMRclear <mohamedmahmued128@gmail.com>
2026-03-23 02:33:17 +0200

    Applying validations like empty note body and maximum charcters

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -10,7 +10,20 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm/6 font-medium text-white">Body</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Here is an idea for a note ..."></textarea>
+            <textarea 
+            id="body" 
+            name="body"
+             rows="3"
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+               placeholder="Here is an idea for a note ..."
+              
+              >
+              <?=  isset($_POST['body']) ? $_POST['body'] : ''?>            
+            </textarea>
+
+              <?php if (isset($errors['body'])) : ?>
+                <p class ="text-red-500 text-xs mt-2"> <?=  $errors['body']?></p>
+                <?php endif ?>
           </div>
         </div>
   <div class="mt-6 flex items-center justify-end gap-x-6">
--- views/note.view.php
+++ views/note.view.php
@@ -6,7 +6,7 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
     <p class = "mb-6"> <a href ="/notes" class ="text-blue-500 undeline" > go back </a> </p>
-         <p> <?=  $note['body'] ?> </p>
+         <p> <?=  htmlspecialchars($note['body']) ?> </p>
     </div>
   </main>
   <?php require ('partial/footer.php') ?>
