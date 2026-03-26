50dab0c4fc7fe57de95a0664839fd0f1977c5287
MMRclear <mohamedmahmued128@gmail.com>
2026-03-26 20:24:57 +0200

    Refactoring notes files to be cleaner

---
+++ controllers/notes/show.php
@@ -0,0 +1,16 @@
+<?php 
+
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+$heading = 'Note';
+
+$current_user_id = 1;
+
+$note = $db -> query('Select * from notes where id = :id',
+[
+    'id' => $_GET['id']])->findOrfail();
+
+authorize($note['user_id'] === $current_user_id);
+require "views/notes/show.view.php";
--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,6 +1,6 @@
- <?php require ('partial/head.php') ?>
-<?php require ('partial/nav.php') ?>
-<?php require ('partial/banner.php') ?>
+<?php require ('views/partial/head.php'); ?>
+<?php require ('views/partial/nav.php'); ?>
+<?php require ('views/partial/banner.php'); ?>
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -34,4 +34,4 @@
 
   </div>
   </main>
-  <?php require ('partial/footer.php') ?>
+  <?php require ('views/partial/footer.php') ?>
